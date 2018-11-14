e0e8b7b324786805543e980c1d0b6df7b4b1ad61
TRand2 <tylerrandolph123@gmail.com>
2018-11-14 12:33:35 -0700

    Added checkstyle

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,30 +3,103 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * Chat Client Class.
+ */
 public class ChatClient extends Application {
 
-    private ChatClient.MessageReader messageReader;
+    /**
+     * Message Reader for Chat Client.
+     */
+    private ChatClient.MessageReader mr;
+
+    /**
+     * Text Area Initializer.
+     */
     private TextArea area;
+
+    /**
+     * Text Field Initializer.
+     */
     private TextField field;
+
+    /**
+     * Socket Initializer.
+     */
     private Socket socket;
+
+    /**
+     * Scanner Initializer.
+     */
     private Scanner input;
+
+    /**
+     * Print Writer Initializer.
+     */
     private PrintWriter output;
+
+    /**
+     * Username Initializer.
+     */
     private static String userName;
+
+    /**
+     * Port number Initializer.
+     */
     private static int portNumber;
 
+    /**
+     * Padding number.
+     */
+    private static final int PADDING = 5;
+
+    /**
+     * Preferred width number.
+     */
+    private static final int PREF_WIDTH = 600;
+
+    /**
+     * Preferred height number.
+     */
+    private static final int PREF_HEIGHT = 400;
+
+    /**
+     * scene width number.
+     */
+    private static final int SCENE_WIDTH = 850;
+
+    /**
+     * scene height number.
+     */
+    private static final int SCENE_HEIGHT = 400;
+
+    /**
+     * Default port number.
+     */
+    private static final int DEFAULT_PORT = 4688;
+
     @Override
-    public void start(Stage stage) {
+    /**
+     * start method to start JavaFX
+     */
+    public void start(final Stage stage) {
 
         BorderPane fieldPane = new BorderPane();
-        fieldPane.setPadding(new Insets(5, 5, 5, 5));
+        fieldPane.setPadding(new Insets(PADDING, PADDING, PADDING, PADDING));
         fieldPane.setLeft(new Label("Enter text: "));
 
         field = new TextField();
@@ -37,7 +110,7 @@ public class ChatClient extends Application {
         area = new TextArea();
         area.setEditable(false);
         area.setWrapText(true);
-        area.setPrefSize(600, 400);
+        area.setPrefSize(PREF_WIDTH, PREF_HEIGHT);
         mainPane.setCenter(new ScrollPane(area));
         mainPane.setBottom(fieldPane);
 
@@ -51,7 +124,7 @@ public class ChatClient extends Application {
 
 
         toolBar.getItems().addAll(sendBtn, disconnectBtn);
-        Scene scene = new Scene(mainPane, 850, 400);
+        Scene scene = new Scene(mainPane, SCENE_WIDTH, SCENE_HEIGHT);
         stage.setTitle("Client " + userName);
         stage.setScene(scene);
         stage.show();
@@ -60,10 +133,14 @@ public class ChatClient extends Application {
 
     }
 
-    public static void main(String[] args) {
+    /**
+     * Main function.
+     * @param args args
+     */
+    public static void main(final String[] args) {
 
         userName = "Anonymous";
-        portNumber = 4688;
+        portNumber = DEFAULT_PORT;
 
         if (args.length > 0) {
             userName = args[0];
@@ -75,22 +152,29 @@ public class ChatClient extends Application {
         launch(args);
     }
 
+
+    /**
+     * connect method for connecting to server.
+     */
     void connect() {
         try {
             this.socket = new Socket("localhost", portNumber);
             this.input = new Scanner(this.socket.getInputStream());
             this.output = new PrintWriter(this.socket.getOutputStream(), true);
-            this.messageReader = new ChatClient.MessageReader();
-            this.messageReader.start();
+            this.mr = new ChatClient.MessageReader();
+            this.mr.start();
             this.output.println("connect " + userName);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * disconnect method for disconnecting from server.
+     */
     void disconnect() {
         this.output.println("disconnect " + userName);
-        this.messageReader.disconnect();
+        this.mr.disconnect();
 
         try {
             this.socket.close();
@@ -101,6 +185,9 @@ public class ChatClient extends Application {
         System.exit(0);
     }
 
+    /**
+     * send message to server.
+     */
     void sendMessage() {
         String text = this.field.getText();
         this.addMessage("Me: " + text + "\n");
@@ -108,18 +195,33 @@ public class ChatClient extends Application {
         this.field.setText("");
     }
 
-    synchronized void addMessage(String msg) {
+    /**
+     * add message to text field.
+     * @param msg msg
+     */
+    synchronized void addMessage(final String msg) {
         this.area.appendText(msg);
     }
 
-
+    /**
+     * Message reader that extends the thread class.
+     */
     class MessageReader extends Thread {
-        boolean done = false;
+        /**
+         * done initializer.
+         */
+        private boolean done = false;
 
+        /**
+         * disconnect from server.
+         */
         void disconnect() {
             this.done = true;
         }
 
+        /**
+         * run method.
+         */
         public void run() {
             while (!this.done) {
                 String msg = this.read();
@@ -129,7 +231,10 @@ public class ChatClient extends Application {
             }
 
         }
-
+        /**
+         * read method.
+         * @return null null
+         */
         String read() {
             if (ChatClient.this.input.hasNextLine()) {
                 return ChatClient.this.input.nextLine();
